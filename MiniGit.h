0a2f7754f5e25395bad11d4ebe7f4386028b0e53
tarikuadugna <tarikuadugna31@gmail.com>
2025-06-24 04:09:55 -0400

    added merge

--- MiniGit.h
+++ MiniGit.h
@@ -13,7 +13,7 @@ private:
         std::string content;
         std::string filename;
     };
-
+    
     struct Commit {
         std::string hash;
         std::string message;
@@ -22,32 +22,45 @@ private:
         std::vector<std::string> blobHashes;
         std::vector<std::string> filenames;
     };
-
+    
+    // Directory and file paths
     const std::string minigitDir = ".minigit";
     const std::string objectsDir = minigitDir + "/objects";
     const std::string refsDir = minigitDir + "/refs";
     const std::string headFile = minigitDir + "/HEAD";
     const std::string indexFile = minigitDir + "/index";
-
+    
+    // Repository state
     std::map<std::string, std::string> branches;
     std::set<std::string> stagedFiles;
     std::string currentBranch = "master";
-
+    
+    // Core utility methods
     std::string computeHash(const std::string& content);
     std::string getCurrentTime();
     void writeToFile(const std::string& filepath, const std::string& content);
     std::string readFromFile(const std::string& filepath);
-
+    
+    // Commit management
     void saveCommit(const Commit& commit);
     Commit loadCommit(const std::string& hash);
     void updateHEAD(const std::string& commitHash);
     std::string getHEAD();
+    
+    // Branch and staging management
     void loadBranches();
+    void saveBranches();
     void loadStagedFiles();
     void saveStagedFiles();
-    void saveBranches();
-
+    
+    // Merge helper methods
+    void cleanupMergeState();
+    std::string findCommonAncestor(const std::string& commit1, const std::string& commit2);
+    std::map<std::string, std::string> getCommitFiles(const std::string& commitHash);
+    bool filesAreSame(const std::string& hash1, const std::string& hash2);
+    
 public:
+    // Public interface methods
     void init();
     void add(const std::string& filename);
     void commit(const std::string& message);
